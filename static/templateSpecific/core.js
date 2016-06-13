8ee8a75e0e10fd9e2d58ae5132307b28c44fa8c7
Robin Malhotra <me@rmalhotra.com>
2016-06-13 18:50:02 +0530

    working finally

--- static/templateSpecific/core.js
+++ static/templateSpecific/core.js
@@ -14,10 +14,25 @@ $('h1,li,.name,.email,.phone,.schoolname,.degree,.company,.location,.companydesc
 
 editable.on('change', elem => {
     const currentContent = editable.getContent(elem);
-    var scope = angular.element($("body")).scope();
+    var scope = angular.element(elem).scope();
+    var ul = $(elem).parent().attr('scopeVal')
+    var li = $(elem)
     scope.$apply(function(){
-        var path = "resumeData."+($(elem).attr("ng-model"))
-        scope[path] = editable.getContent(elem);
-        console.log(path + " has been updated to " + scope[path]);
+        var path = ($(elem).attr("scopeVal"))
+        _.set(scope,"resumeData." + path.split(' ').join('.'),editable.getContent(elem));
     })
+})
+.on('blur', elem => {
+  // your code...
+  const currentContent = editable.getContent(elem);
+  var scope = angular.element(elem).scope();
+  var path = ($(elem).attr("scopeVal"))
+  var ul = $(elem).parent().attr('scopeVal')
+  scope.$apply(function(){
+
+      if(path.substring(0,7) == "bullets")
+      {
+          _.set(scope,ul.split(' ').join('.') +  path.split(' ').join('.'),editable.getContent(elem));
+      }
+  })
 })
