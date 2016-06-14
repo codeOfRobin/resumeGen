d9f7c67e65e519fe0817940cd62c5cdb91ab1556
Robin Malhotra <me@rmalhotra.com>
2016-06-14 13:24:30 +0530

    0 tech debt

--- static/templateSpecific/core.js
+++ static/templateSpecific/core.js
@@ -1,38 +1,35 @@
-var editable = new Editable()
-$('h1,li,.name,.email,.phone,.schoolname,.degree,.company,.location,.companydescription,.designation,.text').each(function(index)
+$('h1,li,.name,.email,.phone,.schoolname,.degree,.company,.location,.companydescription,.designation,.text').map(function(elem)
 {
-    editable.add($(this))
-    // var popper = new Popper($(this),
-    //     {
-    //         content: $(this).attr('ng-model')
-    //     },
-    //     {
-    //     placement: 'left',
-    //     boundariesElement: $(this)
-    // });
+    var popper = new Popper(elem,
+        {
+            content: $(elem).attr('ng-model')
+        },
+        {
+        placement: 'left',
+        boundariesElement: $(this)
+    });
 });
 
-editable.on('change', elem => {
-    const currentContent = editable.getContent(elem);
-    var scope = angular.element($("body")).scope();
-    var ul = $(elem).parent().attr('scopeVal')
-    var li = $(elem)
-    scope.$apply(function(){
-        var path = ($(elem).attr("scopeVal"))
-        _.set(scope,"resumeData." + path.split(' ').join('.'),editable.getContent(elem));
-    })
-})
-.on('blur', elem => {
-  // your code...
-  const currentContent = editable.getContent(elem);
-  var scope = angular.element($("body")).scope();
-  var path = ($(elem).attr("scopeVal"))
-  var ul = $(elem).parent().attr('scopeVal')
-  scope.$apply(function(){
+function genPoppers(element, index, array)
+{
+    var p
+    if (index%2 == 0)
+    {
+        p = "left"
+    }
+    else {
+        p = "right"
+    }
+    var popper = new Popper($(element),
+        {
+            content: $(element).attr('ng-model')
+        },
+        {
+        placement: p,
+        boundariesElement: $(element)
+    });
+}
 
-      if(path.substring(0,7) == "bullets")
-      {
-          _.set(scope,ul.split(' ').join('.') +  path.split(' ').join('.'),editable.getContent(elem));
-      }
-  })
-})
+// Notice that index 2 is skipped since there is no item at
+// that position in the array.
+[2, 5, , 9].forEach(logArrayElements);
