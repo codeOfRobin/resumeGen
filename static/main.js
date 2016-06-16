98b50d946cfa8142a4c855cc12985e2641ce8103
Robin Malhotra <me@rmalhotra.com>
2016-06-16 16:13:53 +0530

    changed subdomain

--- static/main.js
+++ static/main.js
@@ -4,6 +4,6 @@ console.log(encodeURIComponent('\uD800\uDFFF'));
 $("button").click(function()
 {
     console.log("button clicked");
-    var newURL = "http://localhost:3000/resume?resumeID=" + encodeURIComponent($("#resumeID").val())+"&template=" + encodeURIComponent($("#template").val())
+    var newURL = "/resume?resumeID=" + encodeURIComponent($("#resumeID").val())+"&template=" + encodeURIComponent($("#template").val())
     window.location.href = newURL
 })
