586f668c15efe25d72e766467d52716c4e5d25b2
Robin Malhotra <me@rmalhotra.com>
2016-06-17 11:45:04 +0530

    email fixed

--- static/templateSpecific/core.js
+++ static/templateSpecific/core.js
@@ -1,20 +1,3 @@
-var toolTip = {
-"resumeData.personalInformation.name" : "Your Name",
-"resumeData.personalInformation.address" : "Your Address",
-"resumeData.personalInformation.phone" : "Your Contact Number",
-"resumeData.personalInformation.email" : "Your University Email Address",
-"educationPoint.schoolname" : "Name of School/College/University",
-"educationPoint.location" : "Location of School/College/University",
-"educationPoint.degree" : "Degree",
-"educationPoint.endyear" : "Year of Completion",
-"bullet.text" : "These are the bullets under the Education Section",
-"experiencePoint.companyname" : "Organization Name",
-"experiencePoint.location" : "Organization Location",
-"experiencePoint.position" : "Your Designation",
-"experiencePoint.data_raw" : "Experience Tenure (January 2001 - June 2005)",
-"bullet.text" : "These are the bullets under Experience Section"
-}
-
 var elems = $('.name, .email, .phone, .schoolname, .degree, .bullet, .companyname, .location, .companydescription, .designation')
 $.each(elems,function(index, value)
 {
